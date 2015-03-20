0b4ec3be921587ed6aa860a4e56fbc7be7f586e1
mcobit <mcobit@gmx.de>
2015-03-20 14:05:42 +0000

    reconstruction

--- retropie_watchdog.sh
+++ retropie_watchdog.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-sudo openvt -c 8 -s -f clear
-sudo openvt -c 8 -s -f echo "Running emulationstation from KODI"
+sudo openvt -c 7 -s -f clear
+sudo openvt -c 7 -s -f echo "Running emulationstation from KODI"
 sleep 2
 while [ true ]; do
 	VAR1="$(pgrep emulatio)"
 		if [ ! "$VAR1" ]; then
-			sudo openvt -c 8 -s -f clear
-			sudo openvt -c 8 -s -f echo "Emulationstation quit... Starting KODI."
+			sudo openvt -c 7 -s -f clear
+			sudo openvt -c 7 -s -f echo "Emulationstation quit... Starting KODI."
 			sleep 1
-			sudo openvt -c 8 -f clear
+			sudo openvt -c 7 -f clear
 			sudo su -c "service mediacenter restart &" &
 			sleep 1
 			exit
