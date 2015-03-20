c31ba70bfd40a81405e5a01919fb871a6e02cf36
mcobit <mcobit@gmx.de>
2015-03-20 12:20:25 +0000

    Some verbosity on start

--- retropie_watchdog.sh
+++ retropie_watchdog.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+sudo openvt -c 8 -s -f clear
+sudo openvt -c 8 -s -f echo "Running emulationstation from KODI"
 sleep 2
-echo "running"
 while [ true ]; do
 	VAR1="$(pgrep emulatio)"
 		if [ ! "$VAR1" ]; then
+			sudo openvt -c 8 -s -f clear
+			sudo openvt -c 8 -s -f echo "Emulationstation quit... Starting KODI."
 			sleep 1
+			sudo openvt -c 8 -f clear
 			sudo su -c "service mediacenter restart &" &
 			sleep 1
 			exit
