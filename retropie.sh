49b5e7678825ca75694c015119f4c3e13e378a88
mcobit <mcobit@gmx.de>
2015-03-23 20:39:09 +0000

    eliminated the need for a custom mediacenter script. All magic now happens in the retropie.sh file.

--- retropie.sh
+++ retropie.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+# This scripts starts the emulationstation watchdog deamon and
+# emulationstation itself while stopping KODI afterwards.
+# Script by mcobit
 
-touch /home/osmc/retropie_start
 sudo su -c "nohup service mediacenter stop &" &
-sleep 10
 
+sudo openvt -c 7 -s -f clear
+sudo openvt -c 7 -s -f echo "Running emulationstation from KODI"
+
+sudo su osmc -c "sh /home/osmc/retropie_watchdog.sh &" &
+#sleep 1
+sudo su osmc -c "nohup openvt -c 7 -f -s emulationstation &" &
+sleep 1
+#sudo su -c "nohup service mediacenter stop &" &
+
+exit
