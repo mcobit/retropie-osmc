d5ecd166063412cddec0060a649ff3941bfa9d5f
mcobit <mcobit@gmx.de>
2015-03-24 15:19:33 +0000

    directed output of nohup to /dev/null to not create a nohup.out file

--- retropie.sh
+++ retropie.sh
@@ -6,7 +6,7 @@
 sudo openvt -c 7 -s -f clear
 sudo openvt -c 7 -s -f echo "Running emulationstation from KODI"
 sudo su osmc -c "sh /home/osmc/retropie_watchdog.sh &" &
-sudo su osmc -c "nohup openvt -c 7 -f -s emulationstation &" &
+sudo su osmc -c "nohup openvt -c 7 -f -s emulationstation >/dev/null 2>&1 &" &
 sudo openvt -c 7 -s -f clear
 sleep 2
 sudo su -c "systemctl stop mediacenter &" &
