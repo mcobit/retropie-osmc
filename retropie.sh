b8bec6b00f0086f1f013632d9f9f3e66c97a5943
mcobit <mcobit@gmx.de>
2015-03-24 15:14:34 +0000

    Removed some sleep lines to speed up emulationstation startup

--- retropie.sh
+++ retropie.sh
@@ -5,12 +5,10 @@
 
 sudo openvt -c 7 -s -f clear
 sudo openvt -c 7 -s -f echo "Running emulationstation from KODI"
-sleep 2
 sudo su osmc -c "sh /home/osmc/retropie_watchdog.sh &" &
 sudo su osmc -c "nohup openvt -c 7 -f -s emulationstation &" &
-sleep 2
 sudo openvt -c 7 -s -f clear
-
+sleep 2
 sudo su -c "systemctl stop mediacenter &" &
 
 exit
